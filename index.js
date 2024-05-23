2d218c14f69ee77ed2424504295112677dfe3b7e
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-05-23 19:47:15 +1000

    sliding to unlock for app screen

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // console.log('script connected');check
+
+// slide to next (app) screen
+const sliderElm = document.getElementById('mySlider');
+// console.log(sliderElm);
+
+sliderElm.addEventListener('change', (e) => {
+  //   console.log(e.target.value);
+  const { value } = e.target;
+  const labelElm = document.getElementById('label');
+  if (value > 70) {
+    labelElm.textContent = '';
+    displayAppScreen();
+  } else {
+    labelElm.textContent = 'Slide To Unlock';
+  }
+});
+
+const displayAppScreen = () => {
+  // hide home screen
+  //   document.querySelector('.homeScreen').style.display = 'none';
+  document.querySelector('.homeScreen').remove();
+
+  // show app screen
+  document.querySelector('.appScreen').style.display = 'block';
+};
