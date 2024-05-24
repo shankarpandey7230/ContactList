934569ac9c4344ce1d8b7e88acc48147d1c7a92e
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-05-24 18:49:27 +1000

    displaying contacts in UI

--- index.js
+++ index.js
@@ -50,5 +50,83 @@ const fetchContacts = async (url) => {
   userList = data.results;
   // console.log(data);
   console.log(userList);
+  // hide the spinner
+  document.querySelector('.showSpinner').style.display = 'none';
+
+  // display contacts
+  displayContList(userList);
 };
 fetchContacts(apiEp);
+
+// displayContact List
+
+const displayContList = (userList) => {
+  document.getElementById('list').style.display = 'block';
+
+  let str = '';
+  userList.map((item, i) => {
+    str += ` 
+    <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button
+        class="accordion-button collapsed"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#collapse${i}"
+        aria-expanded="false"
+        aria-controls="collapse${i}"
+      >
+        <img
+          src="${item.picture.large}"
+          alt=""
+          width="50px"
+          class="rounded-circle"
+        />
+        <div class="ms-2">
+          <div class="fw-bolder">${item.name.title}  ${item.name.first}  ${item.name.last}</div>
+          <small> ${item.location.street.number} ${item.location.street.name} </small>
+        </div>
+      </button>
+    </h2>
+    <div
+      id="collapse${i}"
+      class="accordion-collapse collapse"
+      data-bs-parent="#accordionExample"
+    >
+      <div
+        class="accordion-body d-flex flex-column align-items-center justify-content-center"
+      >
+        <img
+        src="${item.picture.large}"
+          alt=""
+          width="150px"
+          class="rounded-circle"
+        />
+        <div>
+          <div class="fw-bolder">
+            <i class="bi bi-person-circle"></i>
+            ${item.name.title}  ${item.name.first}  ${item.name.last}
+          </div>
+          <div>
+            <a href="tel:${item.cell}">
+              <i class="bi bi-phone-fill"> ${item.cell}</i> 
+            </a>
+          </div>
+          <div>
+            <a href="mailto:${item.email}"
+              ><i class="bi bi-envelope-fill"></i>${item.email}</a
+            >
+          </div>
+          <div>
+            <a href="https://www.google.com/maps/place/${item.location.street.number} ${item.location.street.name}" target="_blank">
+              <i class="bi bi-globe-asia-australia"></i> ${item.location.street.number} ${item.location.street.name}
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  });
+
+  document.getElementById('accordionExample').innerHTML = str;
+};
