423522df14add39fff6714520833b06d8f658623
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-05-24 18:19:46 +1000

    fetching data from api

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // console.log('script connected');check
 
+const apiEp = 'https://randomuser.me/api?results=2';
+
+let userList = [];
 // slide to next (app) screen
 const sliderElm = document.getElementById('mySlider');
 // console.log(sliderElm);
@@ -24,3 +27,28 @@ const displayAppScreen = () => {
   // show app screen
   document.querySelector('.appScreen').style.display = 'block';
 };
+
+const fetchContacts = async (url) => {
+  // fetch contacts
+  // promise method
+  // fetch(url)
+  //   .then((res) => {
+  //     // console.log(res);
+  //     return res.json();
+  //   })
+  //   .then((data) => {
+  //     console.log(data);
+  //   })
+  //   .catch((error) => {
+  //     console.log(erro)
+  //   });
+
+  // async await;
+  const response = await fetch(url);
+  // console.log(response);
+  const data = await response.json();
+  userList = data.results;
+  // console.log(data);
+  console.log(userList);
+};
+fetchContacts(apiEp);
